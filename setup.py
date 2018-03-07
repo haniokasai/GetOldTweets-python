c7b87906b753e7301e256426d2ab006e4e58f757
lkesko1 <lejla.kesko@hotmail.com>
2018-03-07 17:07:11 +0100

    change setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open('requirements.txt', 'rb') as f:
-    install_requires = f.read()
+with open('requirements.txt') as fp:
+    install_requires = fp.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
